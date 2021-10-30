bc220e4c670bcdc6d39c595292eb3cadfd5c7924
Shubham Jain <shubham.jain20294@gmail.com>
2021-10-30 14:32:00 +0530

    Initial commit

---
+++ src/main/java/com/sfg/clinic/sfgclinic/SfgClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.clinic.sfgclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfg/clinic/sfgclinic/SfgClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.clinic.sfgclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
