e0db75e4ccb62b2eda97538c5c1b748237cb1f34
Shubham Jain <shubham.jain20294@gmail.com>
2021-11-12 14:22:23 +0530

    adding maven release plugin. Closes #3

---
+++ SFG-CLINIC-WEB/src/main/java/com/sfg/clinic/sfgclinic/SfgClinicApplication.java
@@ -0,0 +1,14 @@
+package com.sfg.clinic.sfgclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgClinicApplication.class, args);
+    }
+
+
+}
